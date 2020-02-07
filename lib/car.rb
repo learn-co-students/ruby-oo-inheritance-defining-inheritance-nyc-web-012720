d6cc869453497d65158cc0544609d4b0838f1340
github username <github email address>
2020-02-06 23:18:51 -0500

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+class Car < Vehicle
+    
+    def go 
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!' 
+    end
+
+    def fill_up_tank
+        'filling up!'
+    end
+end
